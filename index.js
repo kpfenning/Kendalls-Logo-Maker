d9688e03701071aa2ff86598573a863d4c0fbb5e
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2023-10-02 15:21:26 -0700

    added some comments and made some corrections to code

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const colors = require('./lib/colors');
-const shapes = require('./lib/shapes');
+const colorsArray = require('./lib/colors.js').colorsArray;
+const colors = require('./lib/colors.js');
+const shapes = require('./lib/shapes.js');
+const { Triangle, Circle, Square } = require('./lib/shapes.js');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 
 const photoWidth = 300;
 const photoHeight = 200;
+
+const textPosition = {
+    x: photoWidth / 2,
+    y: photoHeight / 1.35,
+    'text-anchor': 'middle',
+};
+
 
 inquirer
     .prompt([
@@ -29,7 +38,7 @@ inquirer
             type: 'list',
             name: 'shape',
             message: 'Please choose a shape.',
-            choices: ['Circle', 'Sqaure', 'Triange'],
+            choices: ['Circle', 'Square', 'Triangle'],
         },
         {
             type: 'input',
@@ -44,4 +53,51 @@ inquirer
 
     ])
 
-    
+    .then ((answers) => {
+        let shape;
+        const text = answers.text.toUpperCase();
+        
+        let fontSize;
+        switch (answers.shape) {
+            case 'Circle':
+                const circleRadius = Math.min(photoWidth, photoHeight) * 0.45;
+                shape = new shapes.Circle(photoWidth / 2, photoHeight / 2, circleRadius);
+                fontSize = 30;
+                break;
+            case 'Triangle':
+                const triangleHeight = Math.min(photoWidth, photoHeight) * 1.1;
+                shape = new shapes.Triangle(photoWidth / 2, photoHeight / 2, triangleHeight);
+                fontSize = 25;
+                break;
+            case 'Square':
+                const squareSize = Math.min(photoWidth, photoHeight) * 0.8;
+                shape = new shapes.Square(photoWidth / 2, photoHeight / 2, squareSize);
+                fontSize = 30;
+                break;
+            default:
+                console.error("Invalid shape choice.");
+                process.exit(1);
+        }
+
+        const svgData = `
+            <svg xmlns='http://www.w3.org/2000/svg' width='${photoWidth}' height='${photoHeight}'>
+                ${shape.render(answers.shapeColor)}
+                <text x="${textPosition.x}" y="${textPosition.y}" 
+                    text-anchor="${textPosition['text-anchor']}"
+                    fill="${answers.textColor}" font-size="${fontSize}">
+                    ${text}
+                </text>
+            </svg>`;
+
+        fs.writeFileSync(`${__dirname}/logo.svg`, svgData.toString());
+        console.log('logo.svg file generated!');
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+
+
+
+
+
